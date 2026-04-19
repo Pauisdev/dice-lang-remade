25bbc551a61730231b76823104706dade0245ebd
fabrisdev <ezepuntocom@hotmail.com>
2026-04-19 10:00:12 -0300

    implement traits to match rust style better

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use crate::tokenizer::generate_tree;
+use crate::tokenizer::Tokenize;
+
 mod args;
 mod parser;
 mod tokenizer;
@@ -6,9 +7,16 @@ mod tokenizer;
 fn main() -> Result<(), lexopt::Error> {
     let mut args = args::parse_args()?;
     args.file_text.retain(|c| c != '\n');
-    let tokens = tokenizer::tokenize(args.file_text);
+
+    let tokens = args.file_text.tokenize();
+    //let nodes = parser::parse(tokens);
+
     if args.tree_view {
-        generate_tree(tokens);
+        println!("Generating tree below...");
+        println!("{}", tokens);
+        println!(
+            "Tree generated! Visit https://vgarciasc.github.io/tree-renderer/ and paste it to view it."
+        )
     }
 
     Ok(())
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,70 +1,3 @@
-pub fn tokenize(file_text: String) -> Vec<Token> {
-    let mut stack = String::new();
-    let mut tokens = vec![];
-    let mut quotes_open = false;
-    for i in 0..file_text.len() - 1 {
-        let bytes = file_text.as_bytes();
-        let char = bytes[i] as char;
-        let next_char = if i + 1 > file_text.len() - 1 {
-            None
-        } else {
-            let char = bytes[i + 1] as char;
-            Some(char)
-        };
-        if char == ' ' && !quotes_open {
-            continue;
-        }
-        if next_char.is_none() || next_char.unwrap() == ' ' {
-            let token = match stack.as_str() {
-                "let" => Some(Token::Let),
-                "loop" => Some(Token::Loop),
-                _ => None,
-            };
-            if let Some(token) = token {
-                tokens.push(token);
-                stack.clear();
-            }
-        }
-
-        let token = match char {
-            '=' => Token::Equals,
-            ';' => Token::Semicolon,
-            '(' => Token::OpenParenthesis,
-            ')' => Token::CloseParenthesis,
-            '{' => Token::OpenBracket,
-            '}' => Token::CloseBracket,
-            '+' => Token::Plus,
-            '"' => {
-                quotes_open = !quotes_open;
-                if !quotes_open {
-                    tokens.push(Token::Str(stack.clone()));
-                    stack.clear();
-                }
-                continue;
-            }
-            _ => {
-                stack.push(char);
-                continue;
-            }
-        };
-        if !stack.is_empty() {
-            tokens.push(Token::Identifier(stack.clone()));
-            stack.clear();
-        }
-        tokens.push(token);
-    }
-    let tokens: Vec<Token> = tokens
-        .into_iter()
-        .map(|token| match token {
-            Token::Identifier(value) => match value.parse::<i32>() {
-                Ok(num) => Token::Num(num),
-                Err(_) => Token::Identifier(value),
-            },
-            _ => token,
-        })
-        .collect();
-    tokens
-}
 #[derive(Debug, PartialEq)]
 pub enum Token {
     Let,
@@ -81,12 +14,91 @@ pub enum Token {
     CloseBracket,
 }
 
-pub fn generate_tree(tokens: Vec<Token>) {
-    println!("Generating tree below...");
-    for (i, token) in tokens.iter().enumerate() {
-        println!("{} {:?}", "-".repeat(i + 1), token);
+pub trait Tokenize {
+    fn tokenize(self) -> Tokens;
+}
+
+impl Tokenize for String {
+    fn tokenize(self) -> Tokens {
+        let mut stack = String::new();
+        let mut tokens = vec![];
+        let mut quotes_open = false;
+        for i in 0..self.len() - 1 {
+            let bytes = self.as_bytes();
+            let char = bytes[i] as char;
+            let next_char = if i + 1 > self.len() - 1 {
+                None
+            } else {
+                let char = bytes[i + 1] as char;
+                Some(char)
+            };
+            if char == ' ' && !quotes_open {
+                continue;
+            }
+            if next_char.is_none() || next_char.unwrap() == ' ' {
+                let token = match stack.as_str() {
+                    "let" => Some(Token::Let),
+                    "loop" => Some(Token::Loop),
+                    _ => None,
+                };
+                if let Some(token) = token {
+                    tokens.push(token);
+                    stack.clear();
+                }
+            }
+
+            let token = match char {
+                '=' => Token::Equals,
+                ';' => Token::Semicolon,
+                '(' => Token::OpenParenthesis,
+                ')' => Token::CloseParenthesis,
+                '{' => Token::OpenBracket,
+                '}' => Token::CloseBracket,
+                '+' => Token::Plus,
+                '"' => {
+                    quotes_open = !quotes_open;
+                    if !quotes_open {
+                        tokens.push(Token::Str(stack.clone()));
+                        stack.clear();
+                    }
+                    continue;
+                }
+                _ => {
+                    stack.push(char);
+                    continue;
+                }
+            };
+            if !stack.is_empty() {
+                tokens.push(Token::Identifier(stack.clone()));
+                stack.clear();
+            }
+            tokens.push(token);
+        }
+        let tokens: Vec<Token> = tokens
+            .into_iter()
+            .map(|token| match token {
+                Token::Identifier(value) => match value.parse::<i32>() {
+                    Ok(num) => Token::Num(num),
+                    Err(_) => Token::Identifier(value),
+                },
+                _ => token,
+            })
+            .collect();
+        Tokens(tokens)
     }
-    println!(
-        "Tree generated! Visit https://vgarciasc.github.io/tree-renderer/ and paste it to view it."
-    )
+}
+
+pub struct Tokens(Vec<Token>);
+
+impl std::fmt::Display for Tokens {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let output: Vec<String> = self
+            .0
+            .iter()
+            .enumerate()
+            .map(|(i, token)| format!("{} {:?}", "-".repeat(i + 1), token))
+            .collect();
+        let output = output.join("\n");
+        write!(f, "{}", output)
+    }
 }
